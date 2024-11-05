7f69a480e739bdf1bbb71a7a45471d3ed131d10f
Darlan Thiago <tdarlan1@hotmail.com>
2024-11-05 17:38:41 -0300

    first commit

---
+++ apps/react-vite/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@repo/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1>React with vite</h1>
+      <Button appName="react-vite">
+        Click me
+      </Button>
+    </div>
+  );
+}
+
+export default App;
